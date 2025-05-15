6f6d3c1c46ee36a420d82660cbd1f96bdbd13c7a
Umarali56 <maniyarumar3421@gmail.com>
2025-05-16 00:41:52 +0530

    Secoend Commit.

--- HelloDemo/src/HelloWorld.java
+++ HelloDemo/src/HelloWorld.java
@@ -2,8 +2,13 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello");
+		
+		for(int i=0; i<=9; i++)
+		{
+			System.out.println("Umar..");
+		}
 	}
 
 }
